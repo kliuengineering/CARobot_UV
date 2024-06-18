44e90560e2677eea46e2e0f94b7b2fc6ccade1fc
Kevin <kliuengineering@gmail.com>
2024-06-18 16:46:42 -0400

    added test codes

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from machine import I2C, Pin
 from ssd1306 import SSD1306_I2C
+import time
 from time import sleep
 
 
-# The following section of codes test the SSD1306 display
-# Can comment it out inside the main() section
-def TestSSD1306():
+# Tests the I2C connection @ pin 17 and 16
+def TestI2C():
+    i2c = I2C(0, scl=Pin(17), sda=Pin(16))
+    while True:
+        devices = i2c.scan()
+        if devices:
+            for device in devices:
+                print("I2C device found at address:", hex(device))
+        else:
+            print("No I2C devices found")
+        time.sleep(2)
 
+
+# The following section of code tests the SSD1306 display
+def TestSSD1306():
     # Initialize I2C interface
     i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 
@@ -18,7 +30,7 @@ def TestSSD1306():
 
     # Display some text
     oled.text("Hello, World!", 0, 0)
-    oled.text("MicroPython", 0, 10)
+    oled.text("Canada Robotix", 0, 10)
     oled.text("SSD1306 OLED", 0, 20)
 
     # Update the display to show the text
@@ -29,6 +41,9 @@ def TestSSD1306():
         sleep(1)
 
 
-# main() 
-if __name__ == "__main__":
-    TestSSD1306()
+# Uncomment the following line to test the I2C connection
+# TestI2C()
+
+# Uncomment the following line to test the SSD1306 display
+TestSSD1306()
+
